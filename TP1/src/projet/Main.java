5285e6024d2a318807d1cd46ed3143ebcbc4715c
Vivien Maugarnis <>
2021-09-26 17:17:20 +0200

    First buttons (close and help) for the horizontal menu

--- TP1/src/projet/Main.java
+++ TP1/src/projet/Main.java
@@ -7,12 +7,16 @@ import javax.swing.JPanel;
 import javax.swing.BorderFactory;
 import java.awt.BorderLayout;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 import javax.swing.JMenu;
 import java.awt.Graphics;
 import javax.imageio.ImageIO;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Main {
 
@@ -26,10 +30,11 @@ public class Main {
     private JFrame frame; // La fenêtre
     private JMenuBar menuBar; // Menu en haut horizontal
     private Image imgFond; // Image de fond du graph
+    private JMenuItem fermer, aide;
 
-
-    // CONSTRUCTEUR
+    
     public Main() {
+    // CONSTRUCTEUR
         createView();
     }
 
@@ -42,26 +47,16 @@ public class Main {
     
     private void createView() {
         frame = new JFrame("TP1 - application Swing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 
         JPanel menu1 = new JPanel();
         menu1.setPreferredSize(new Dimension(MENU_WIDTH, MENU_HEIGHT));
         menu1.setBorder(BorderFactory.createEtchedBorder());
-        
         frame.add(menu1, BorderLayout.EAST);
-
-        menuBar = new JMenuBar();
-        JMenu menuFichier = new JMenu("Fichier");
-        // menuFichier.add(fermer);
-        menuBar.add(menuFichier);
-
-        JMenu menuEdition = new JMenu("A propos");
-        // menuEdition.add(aide);
-        menuBar.add(menuEdition);
         
+        this.createHorizontalMenu();
         
-        frame.setJMenuBar(menuBar);
-
         try {
             imgFond = ImageIO.read(new File("images/Fond.png"));
         } catch (IOException e) {
@@ -78,6 +73,33 @@ public class Main {
             }
         };
         frame.add(panelImage);
+    }
+
+    private void createHorizontalMenu(){
+        menuBar = new JMenuBar();
+        JMenu menuFichier = new JMenu("File");
+        fermer = new JMenuItem("Close");
+        menuFichier.add(fermer);
+        menuBar.add(menuFichier);
+
+        JMenu menuEdition = new JMenu("About");
+        aide = new JMenuItem("Help");
+        menuEdition.add(aide);
+        menuBar.add(menuEdition);
+
+        fermer.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        aide.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Contact :\n" + "Vivien : sample@email.fr");
+            }
+        });
+
+        frame.setJMenuBar(menuBar);
     }
 
     // POINT D'ENTREE
