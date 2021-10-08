09cabb2581e6737015d6ab3f14b8dbd7547757a7
Vivien Maugarnis <>
2021-10-08 11:21:50 +0200

    All the app

--- TP4/desktop/src/com/interfacesgraphiqueset3d/tp4/desktop/DesktopLauncher.java
+++ TP4/desktop/src/com/interfacesgraphiqueset3d/tp4/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package com.interfacesgraphiqueset3d.tp4.desktop;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.interfacesgraphiqueset3d.tp4.Application;
+// import com.badlogic.drop.Drop;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		new Lwjgl3Application(new Application(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Drop");
+        config.setWindowedMode(800, 480);
+        new Lwjgl3Application(new Application(), config);
+    }
 }
