16183f3fd84d4325f58be72f81f9bdf2ac5d5f4b
Vivien Maugarnis <>
2021-10-24 18:27:57 +0200

    First empty version TP5

---
+++ TP5/desktop/src/com/interfacesgraphiqueset3d/tp5/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.interfacesgraphiqueset3d.tp5.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.interfacesgraphiqueset3d.tp5.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new Application(), config);
+	}
+}
